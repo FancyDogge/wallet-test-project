17d88503a4544548fb4edd1a35f122355894d92a
FancyDogge <83734369+FancyDogge@users.noreply.github.com>
2022-08-25 04:08:59 +0300

    added welcome_bonus functionality

--- wallet/serializers.py
+++ wallet/serializers.py
@@ -10,5 +10,20 @@ class FullWalletSerializer(serializers.ModelSerializer):
         read_only_fields = ["balance"]
 
     def create(self, validated_data):
+
+        def welcome_bonus(currency_type):
+            bonus_dict = {
+                'RUB': 100,
+                'USD': 3,
+                'EUR': 3,
+            }
+            return bonus_dict[currency_type]
+
         validated_data["owner"] = self.context["request"].user
-        return Wallet.objects.create(**validated_data)
+        new_wallet = Wallet.objects.create(**validated_data)
+        new_wallet.balance = welcome_bonus(new_wallet.currency)
+        new_wallet.save()
+
+        return new_wallet
+
+    
